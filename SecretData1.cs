b70e1ffb8a2a175f991c256203482bf8b5e0e4b6
tormozit <tormozit@gmail.com>
2021-04-26 18:23:15 +0300

    +SecretData stub class

---
+++ SecretData1.cs
@@ -0,0 +1,18 @@
+﻿namespace ClipAngel
+{
+    public static class SecretData1
+    {
+        public static string FirebaseDatabase()
+        {
+            return "";
+        }
+        public static string FirebaseMessaging()
+        {
+            return "";
+        }
+        public static string CryptoIV()
+        {
+            return "";
+        }
+    }
+}
