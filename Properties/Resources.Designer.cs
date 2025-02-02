eda96d65dd4090756c8561aa5d1da161599ed764
tormozit <11896251+tormozit@users.noreply.github.com>
2025-02-02 09:42:10 +0300

    2.17

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -1142,7 +1142,7 @@ namespace ClipAngel.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 2.16.
+        ///   Looks up a localized string similar to 2.17.
         /// </summary>
         public static string VersionValue {
             get {
